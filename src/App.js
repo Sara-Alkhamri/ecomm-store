0028e845b87504476a779b5b980f7748a4d26b0a
Sara Alkhamri <s.alkhamri@gmail.com>
2020-09-23 16:33:24 -0700

    Routes setup in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import NavBar from "./components/NavBar";
@@ -11,10 +12,12 @@ function App() {
   return (
     <React.Fragment>
       <NavBar />
-      <ProductList />
-      <Details />
-      <Cart />
-      <Default />
+      <Switch>
+        <Route exact path="/" component={ProductList} />
+        <Route path="/details" component={Details} />
+        <Route path="/cart" component={Cart} />
+        <Route component={Default} />
+      </Switch>
     </React.Fragment>
   );
 }
