853d03503133049ea5060496c6e43d8c68a3b745
Sara Alkhamri <s.alkhamri@gmail.com>
2020-09-23 18:50:55 -0700

    Added state and handelDetail and addToCart methods to the value object in context provider

--- src/context.js
+++ src/context.js
@@ -1,14 +1,31 @@
 import React, { Component } from "react";
-
+import { stireProducts, detailProduct, storeProducts } from "./data";
 const ProductContext = React.createContext();
 //provider
 //consumer
 
 class ProductProvider extends Component {
+  state = {
+    products: storeProducts,
+    detailProduct: detailProduct
+  };
+  handelDetail = () => {
+    console.log("Hello from Detail");
+  };
+
+  addToCart = () => {
+    console.log("Hello from add to cart");
+  };
   render() {
     return (
-      <ProductContext.Provider value="hello from context">
-        {this.props.childern}
+      <ProductContext.Provider
+        value={{
+          ...this.state,
+          handelDetail: this.handelDetail,
+          addToCart: this.addToCart
+        }}
+      >
+        {this.props.children}
       </ProductContext.Provider>
     );
   }
