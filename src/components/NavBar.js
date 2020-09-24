3c41bae1f3adadae647d416208ce8acb3f6aa203
Sara Alkhamri <s.alkhamri@gmail.com>
2020-09-23 17:45:33 -0700

    Created Button component and added styling using styled components

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,16 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import logo from "../logo.svg";
+import styled from "styled-components";
+import { ButtonContainer } from "./Button";
 
 export default class NavBar extends Component {
   render() {
     return (
       <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
         {/* https://www.iconfinder.com/icons/1243689/call_phone_icon
-Creative Commons (Attribution 3.0 Unported);
-https://www.iconfinder.com/Makoto_msk  */}
+          Creative Commons (Attribution 3.0 Unported);
+          https://www.iconfinder.com/Makoto_msk  */}
         <Link to="/">
           <img src={logo} alt="store" className="navbar-brand" />
         </Link>
@@ -20,10 +22,12 @@ https://www.iconfinder.com/Makoto_msk  */}
           </li>
         </ul>
         <Link to="/cart" className="ml-auto">
-          <button>
-            <i className="fas fa-cart-plus" />
+          <ButtonContainer>
+            <span className="mr-2">
+              <i className="fas fa-cart-plus" />
+            </span>
             My Cart
-          </button>
+          </ButtonContainer>
         </Link>
       </nav>
     );
