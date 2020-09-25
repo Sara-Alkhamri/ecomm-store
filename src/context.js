4812a57e69162279902eb148d50cb3d3db496b8b
Sara Alkhamri <s.alkhamri@gmail.com>
2020-09-24 18:09:44 -0700

    Replaced the storeProducts object assigned to products in context.js to ensure getting a fresh set of data after rerendering

--- src/context.js
+++ src/context.js
@@ -7,9 +7,24 @@ const ProductContext = React.createContext();
 
 class ProductProvider extends Component {
   state = {
-    products: storeProducts,
+    products: [],
     detailProduct: detailProduct
   };
+  componentDidMount() {
+    this.setProducts();
+  }
+
+  setProducts = () => {
+    let tempProducts = [];
+    storeProducts.forEach(item => {
+      const singleItem = { ...item };
+      tempProducts = [...tempProducts, singleItem];
+    });
+    this.setState(() => {
+      return { products: tempProducts };
+    });
+  };
+
   handleDetail = () => {
     console.log("Hello from Detail");
   };
@@ -17,6 +32,7 @@ class ProductProvider extends Component {
   addToCart = () => {
     console.log("Hello from add to cart");
   };
+
   render() {
     return (
       <ProductContext.Provider
