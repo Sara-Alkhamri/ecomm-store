9141d167334955cd413e6c05d3a4e1b683da577b
Sara Alkhamri <s.alkhamri@gmail.com>
2020-09-28 15:14:36 -0700

    Setup cart component placeholder methods in context.js

--- src/context.js
+++ src/context.js
@@ -11,7 +11,10 @@ class ProductProvider extends Component {
     detailProduct: detailProduct,
     cart: [],
     modalOpen: false,
-    modalProduct: detailProduct
+    modalProduct: detailProduct,
+    cartSubtotal: 0,
+    cartTax: 0,
+    cartTotal: 0
   };
   componentDidMount() {
     this.setProducts();
@@ -75,6 +78,22 @@ class ProductProvider extends Component {
     });
   };
 
+  increment = id => {
+    console.log("This is the increment method");
+  };
+
+  decremnt = id => {
+    console.log("This is the decremnt method");
+  };
+
+  removeItem = id => {
+    console.log("item removed");
+  };
+
+  clearCart = () => {
+    console.log("cart was cleared");
+  };
+
   render() {
     return (
       <ProductContext.Provider
@@ -83,7 +102,11 @@ class ProductProvider extends Component {
           handleDetail: this.handleDetail,
           addToCart: this.addToCart,
           openModal: this.openModal,
-          closeModal: this.closeModal
+          closeModal: this.closeModal,
+          increment: this.increment,
+          decremnt: this.decremnt,
+          removeItem: this.removeItem,
+          clearCart: this.clearCart
         }}
       >
         {this.props.children}
