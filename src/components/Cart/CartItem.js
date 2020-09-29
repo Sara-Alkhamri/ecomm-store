4fa4b047f37996ae588450867c321ddd61d672f8
Sara Alkhamri <s.alkhamri@gmail.com>
2020-09-28 17:24:46 -0700

    Completed CartTotals component

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -4,7 +4,7 @@ export default function CartItem({ item, value }) {
   const { id, title, img, price, total, count } = item;
   const { increment, decrement, removeItem } = value;
   return (
-    <div className="row my-1 text-capitalized text-center">
+    <div className="row my-2 text-capitalized text-center">
       <div className="col-10 mx-auto col-lg-2">
         <img
           src={img}
@@ -24,11 +24,23 @@ export default function CartItem({ item, value }) {
       </div>
       <div className="col-10 mx-auto col-lg-2 my-2 my-lg-0">
         <div className="d-flex justify-content-center">
-          <span
-            className="btn btn-black mx-1"
-            onClick={() => decrement(id)}
-          ></span>
+          <span className="btn btn-black mx-1" onClick={() => decrement(id)}>
+            -
+          </span>
+          <span className="btn btn-black mx-1">{count}</span>
+          <span className="btn btn-black mx-1" onClick={() => increment(id)}>
+            +
+          </span>
         </div>
+      </div>
+      <div className="col-10 mx-auto col-lg-2">
+        <div className="cart-icon" onClick={() => removeItem(id)}>
+          <i className="fas fa-trash"></i>
+        </div>
+      </div>
+
+      <div className="col-10 mx-auto col-lg-2">
+        <strong>Item Total : ${total}</strong>
       </div>
     </div>
   );
